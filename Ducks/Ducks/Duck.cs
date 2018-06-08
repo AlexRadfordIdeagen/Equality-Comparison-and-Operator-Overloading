7c5ab6c9347bfdd8f2e03bad16eb7e69efd5b0e3
IDEAGENPLC\Alex.Radford <alex.radford@ideagenplc.com>
2018-06-08 09:00:12 +0100

    Added Ducks to dictionary using a hashcode.

--- Ducks/Ducks/Duck.cs
+++ Ducks/Ducks/Duck.cs
@@ -6,13 +6,30 @@ using System.Threading.Tasks;
 
 namespace Ducks
 {
-    public class Duck
+    public class Duck : IEquatable<Duck>
     {
         public string Name { get; }
         public string Type { get; }
         public int WeightInGrams { get; }
         public int AgeInMonths { get; }
 
+        public override int GetHashCode()
+        {
+            int hash = Name.GetHashCode();
+            hash = (hash * 23) + Type.GetHashCode();
+            hash = (hash * 23) + WeightInGrams;
+            hash = (hash * 23) + AgeInMonths;
+
+            return hash;
+        }
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Duck);
+        }
+        public bool Equals(Duck obj)
+        {
+            return obj != null && obj.Name == Name;
+        }
 
         public Duck(string name, string type, int weightInGrams, int ageInMonths)
         {
--- Ducks/Ducks/Program.cs
+++ Ducks/Ducks/Program.cs
@@ -11,6 +11,22 @@ namespace Ducks
         static void Main(string[] args)
         {
             var quack = new Duck("Sir Quackalot", "Mallard", 100, 17);
+            var quackers = new Duck("Miss Quack", "Marbled Duck", 38, 11);
+            var quacked = new Duck("Sir Quackalot", "Mallard", 100, 14);
+
+            Console.WriteLine($"{quack.Name} has the hash code of {quack.GetHashCode()}");
+            Console.WriteLine($"{quackers.Name} has the hash code of {quackers.GetHashCode()}");
+            Console.WriteLine($"{quacked.Name} has the hash code of {quacked.GetHashCode()}");
+
+            Dictionary<Duck, int> ducks = new Dictionary<Duck, int>();
+
+            ducks.Add(quack, quack.GetHashCode());
+            ducks.Add(quackers, quackers.GetHashCode());
+            ducks.Add(quacked, quacked.GetHashCode());
+
+
+ 
+            Console.ReadLine();
         }
     }
 }
