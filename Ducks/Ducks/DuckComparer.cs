4fb6e01b6ddb8c7d4252e6ea93139c6871a6c61e
IDEAGENPLC\Alex.Radford <alex.radford@ideagenplc.com>
2018-06-08 10:20:06 +0100

    Implemented sorting for Duck class based on name and then on weight.

---
+++ Ducks/Ducks/DuckComparer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    public class DuckComparer : IComparer<Duck>
+    {
+        public  int Compare(Duck x, Duck y)
+        {
+            Console.WriteLine($"{x.Name} is being compared to {y.Name}");
+            if (String.Compare(x.Name, y.Name, true) == 0)
+            {
+                Console.WriteLine($"{x.Name} and {y.Name} are the same so we are ordering them now by weight");
+                if (x.WeightInGrams > y.WeightInGrams)
+                {
+                    Console.WriteLine($"{x.Name} is above in weight by {x.WeightInGrams} to {y.Name}'s {y.WeightInGrams}");
+
+                    return 1;
+                }
+
+                else
+                {
+                    Console.WriteLine($"{y.Name} is above in weight by {y.WeightInGrams} to {x.Name}'s {x.WeightInGrams}");
+
+                    return -1;
+                }
+            }
+            else
+            {
+                if (String.Compare(x.Name, y.Name, true) == 1)
+                {
+                    Console.WriteLine($"{x.Name} is higher in the alphabet compared to {y.Name}");
+
+                }
+                if (String.Compare(x.Name, y.Name, true) == -1)
+                {
+                    Console.WriteLine($"{y.Name} is higher in the alphabet compared to {x.Name}");
+
+                }
+                return String.Compare(x.Name, y.Name, true);
+            }
+        }
+    }
+}
--- Ducks/Ducks/Program.cs
+++ Ducks/Ducks/Program.cs
@@ -12,7 +12,7 @@ namespace Ducks
         {
             var quack = new Duck("Sir Quackalot", "Mallard", 100, 17);
             var quackers = new Duck("Miss Quack", "Marbled Duck", 38, 11);
-            var quacked = new Duck("Sir Quackalot", "Mallard", 100, 14);
+            var quacked = new Duck("Sir Quackalot", "Mallard", 99, 14);
 
             Console.WriteLine($"{quack.Name} has the hash code of {quack.GetHashCode()}");
             Console.WriteLine($"{quackers.Name} has the hash code of {quackers.GetHashCode()}");
@@ -25,7 +25,22 @@ namespace Ducks
             ducks.Add(quacked, quacked.GetHashCode());
 
 
- 
+            List<Duck> duckies = new List<Duck>();
+
+            duckies.Add(quack);
+            duckies.Add(quackers);
+            duckies.Add(quacked);
+            Console.WriteLine("The ducks have now been added to a list and sorted in name");
+            foreach (var duck in duckies)
+            {
+                Console.WriteLine(duck.Name);
+            }
+            duckies.Sort(new DuckComparer());
+
+            foreach (var duck in duckies)
+            {
+                Console.WriteLine("Name: {0} Type: {1} Weight in grams: {2} Age in months: {3}", duck.Name, duck.Type, duck.WeightInGrams, duck.AgeInMonths);
+            }
             Console.ReadLine();
         }
     }
