e22fb6fe472ced3f638a7e38eddaa171c7073bb3
IDEAGENPLC\Alex.Radford <alex.radford@ideagenplc.com>
2018-06-08 08:35:18 +0100

    Initialize of the training

---
+++ Ducks/Ducks/Duck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    public class Duck
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public int WeightInGrams { get; }
+        public int AgeInMonths { get; }
+
+
+        public Duck(string name, string type, int weightInGrams, int ageInMonths)
+        {
+            Name = name;
+            Type = type;
+            WeightInGrams = weightInGrams;
+            AgeInMonths = ageInMonths;
+        }
+    }
+
+}
